652a0496b103b64f2d5a3b4882ffa4c7db6ce459
Lasantha Basnayake <lasantha57@gmail.com>
2020-01-13 02:07:02 +0800

    Convert to react-hooks

--- src/App.js
+++ src/App.js
@@ -1,58 +1,99 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
-class App extends Component {
+const App = () => {
 
-  state = {
-    liked: false,
-    disliked: false,
-    likedCount: 100,
-    dislikedCount: 25
-  }
+  const [liked, setLike] = useState(false);
+  const [disliked, setDislike] = useState(false);
+  const [likedCount, setLikeCount] = useState(100);
+  const [dislikedCount, setDislikeCount] = useState(25);
 
-  likeHandler = () => {
-    let { liked, likedCount, disliked, dislikedCount } = this.state;
-    liked = !liked;
+  const likeHandler = () => {
+    let likeStatus = !liked;
 
-    if (liked && disliked) {
-      disliked = false;
-      dislikedCount--;
+    setLike(likeStatus);
+
+    if (likeStatus && disliked) {
+      setDislike(false);
+      setDislikeCount(dislikedCount - 1);
     }
 
-    likedCount = liked ? likedCount + 1 : likedCount - 1;
-
-    this.setState({
-      liked, likedCount, disliked, dislikedCount
-    });
+    setLikeCount(likeStatus ? likedCount + 1 : likedCount - 1);
   }
 
-  disLikeHanlder = () => {
-    let { liked, likedCount, disliked, dislikedCount } = this.state;
+  const disLikeHanlder = () => {
+    let disLikeStatus = !disliked;
 
-    disliked = !disliked;
+    setDislike(disLikeStatus);
 
-    if (disliked && liked) {
-      liked = false;
-      likedCount--;
+    if (disLikeStatus && liked) {
+      setLike(false);
+      setLikeCount(likedCount - 1);
     }
 
-    dislikedCount = disliked ? dislikedCount + 1 : dislikedCount - 1;
-
-    this.setState({
-      liked, likedCount, disliked, dislikedCount
-    });
+    setDislikeCount(disLikeStatus ? dislikedCount + 1 : dislikedCount - 1);
   }
 
-  render() {
-    const { liked, disliked, likedCount, dislikedCount } = this.state;
-    return (
-      <div>
-        <button className={liked ? 'like-button liked' : 'like-button'} onClick={this.likeHandler}>Like | {likedCount}</button>
-        <button className={disliked ? 'dislike-button disliked' : 'dislike-button'} onClick={this.disLikeHanlder}>Dislike | {dislikedCount}</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <button className={liked ? 'like-button liked' : 'like-button'} onClick={likeHandler}>Like | {likedCount}</button>
+      <button className={disliked ? 'dislike-button disliked' : 'dislike-button'} onClick={disLikeHanlder}>Dislike | {dislikedCount}</button>
+    </div>
+  );
 }
+
+// class App extends Component {
+
+//   state = {
+//     liked: false,
+//     disliked: false,
+//     likedCount: 100,
+//     dislikedCount: 25
+//   }
+
+//   likeHandler = () => {
+//     let { liked, likedCount, disliked, dislikedCount } = this.state;
+//     liked = !liked;
+
+//     if (liked && disliked) {
+//       disliked = false;
+//       dislikedCount--;
+//     }
+
+//     likedCount = liked ? likedCount + 1 : likedCount - 1;
+
+//     this.setState({
+//       liked, likedCount, disliked, dislikedCount
+//     });
+//   }
+
+//   disLikeHanlder = () => {
+//     let { liked, likedCount, disliked, dislikedCount } = this.state;
+
+//     disliked = !disliked;
+
+//     if (disliked && liked) {
+//       liked = false;
+//       likedCount--;
+//     }
+
+//     dislikedCount = disliked ? dislikedCount + 1 : dislikedCount - 1;
+
+//     this.setState({
+//       liked, likedCount, disliked, dislikedCount
+//     });
+//   }
+
+//   render() {
+//     const { liked, disliked, likedCount, dislikedCount } = this.state;
+//     return (
+//       <div>
+//         <button className={liked ? 'like-button liked' : 'like-button'} onClick={this.likeHandler}>Like | {likedCount}</button>
+//         <button className={disliked ? 'dislike-button disliked' : 'dislike-button'} onClick={this.disLikeHanlder}>Dislike | {dislikedCount}</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
